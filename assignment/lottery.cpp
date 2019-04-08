125814f287b0efbd274619880794aa6a9d7f799c
Handstanger Alexander <if170042@WS8A08.local>
2019-04-08 09:39:19 +0200

    builds

--- assignment/lottery.cpp
+++ assignment/lottery.cpp
@@ -10,3 +10,27 @@
  * Implementation of a library for analyzing lottery tips.
  *-----------------------------------------------------------------------------
 */
+#include <stdio.h>
+#include "lottery.h"
+
+bool init_lottery(const char *csv_file, char csv_separator){
+  FILE* stream = fopen(csv_file, "r");
+
+  return false;
+}
+
+bool get_tip(int tip_number, int tip[TIP_SIZE]){
+  return false;
+}
+
+bool set_drawing(int drawing_numbers[TIP_SIZE]){
+  return false;
+}
+
+int get_tip_result(int tip_number){
+  return -1;
+}
+
+int get_right_tips_count(int right_digits_count){
+  return -1;
+}
